049e5b0ff619dd2fcc664d9b49e138afed0b739c
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-21 11:42:25 -0700

    Employee Overview Page

---
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShopHRM.APP.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employees { get; set; } = default!;
+
+        //life cycle method
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
