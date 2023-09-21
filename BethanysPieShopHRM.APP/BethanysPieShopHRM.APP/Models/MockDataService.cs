7820632992e98b2287470a77860d2cf88969056e
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-21 13:59:53 -0700

    add 1 more employee without lastname

--- BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Models/MockDataService.cs
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Models/MockDataService.cs
@@ -71,7 +71,29 @@ namespace BethanysPieShopHRM.App.Models
                 CountryId = _countries[1].CountryId
             };
 
-            return new List<Employee>() { e1, e2 };
+            var e3 = new Employee
+            {
+                MaritalStatus = MaritalStatus.Single,
+                BirthDate = new DateTime(1999, 1, 16),
+                City = "Antwerp",
+                Email = "gill@bethanyspieshop.com",
+                EmployeeId = 2,
+                FirstName = "Gill",
+                Gender = Gender.Female,
+                PhoneNumber = "33999909923",
+                Smoker = false,
+                Street = "New Street",
+                Zip = "2000",
+                JobCategory = _jobCategories[1],
+                JobCategoryId = _jobCategories[1].JobCategoryId,
+                Comment = "Lorem Ipsum",
+                ExitDate = null,
+                JoinedDate = new DateTime(2017, 12, 24),
+                Country = _countries[1],
+                CountryId = _countries[1].CountryId
+            };
+
+            return new List<Employee>() { e1, e2, e3 };
         }
 
         private static List<JobCategory> InitializeMockJobCategories()
