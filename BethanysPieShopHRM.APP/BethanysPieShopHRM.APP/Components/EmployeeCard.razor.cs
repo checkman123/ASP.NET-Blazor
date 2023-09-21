419377d3241ed679a415bbccb8eaae039137f47b
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-21 13:22:33 -0700

    show full employee details

--- BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Components/EmployeeCard.razor.cs
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Components/EmployeeCard.razor.cs
@@ -7,5 +7,7 @@ namespace BethanysPieShopHRM.APP.Components
     {
         [Parameter]
         public Employee Employee { get; set; } = default!;
+        [Parameter]
+        public EventCallback<Employee> EmployeeQuickViewClicked { get; set; }
     }
 }
---
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.APP.Pages
+{
+    public partial class EmployeeDetail
+    {
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+        public Employee? Employee { get; set; } = new Employee();
+
+        protected override Task OnInitializedAsync()
+        {
+            Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
