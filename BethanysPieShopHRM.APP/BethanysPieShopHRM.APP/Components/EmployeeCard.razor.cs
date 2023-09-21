22839439e3ff28e68db8a6c698d8eb92592b0b5c
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-21 12:13:36 -0700

    EmployeeCard component

---
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Components/EmployeeCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.APP.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+    }
+}
