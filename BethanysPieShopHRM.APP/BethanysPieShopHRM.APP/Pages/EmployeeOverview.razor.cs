ff3a6940446dc249329a6fd0377915b27f839f56
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-25 12:52:40 -0700

    inject httpclient directly into component [Not recommended] There is a better approach

--- BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Pages/EmployeeOverview.razor.cs
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Pages/EmployeeOverview.razor.cs
@@ -1,10 +1,14 @@
 ﻿using BethanysPieShopHRM.App.Models;
 using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace BethanysPieShopHRM.APP.Pages
 {
     public partial class EmployeeOverview
     {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
         public List<Employee>? Employees { get; set; } = default!;
         private Employee? _selectedEmployee;
         public string Title = "Employee Overview";
@@ -13,6 +17,10 @@ namespace BethanysPieShopHRM.APP.Pages
         protected override void OnInitialized()
         {
             Employees = MockDataService.Employees;
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            var data = await HttpClient.GetFromJsonAsync<List<Employee>>("https://localhost:7039/api/Employee");
         }
 
         public void ShowQuickViewPopup(Employee selectedEmployee)
