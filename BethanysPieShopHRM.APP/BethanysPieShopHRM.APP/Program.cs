818a061de8e11bda142f62af574b158dd1806003
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-26 16:24:49 -0700

    Secure API with JwtBearer and send token correctly

--- BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Program.cs
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Program.cs
@@ -4,18 +4,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using BethanysPieShopHRM.App.Services;
-
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient<IEmployeeDataService,EmployeeDataService>(client => 
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client =>
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>(); ;
 builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client =>
-    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>(); ;
 
 builder.Services.AddScoped<ApplicationState>();
 
@@ -25,6 +25,7 @@ builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
 });
 
 await builder.Build().RunAsync();
