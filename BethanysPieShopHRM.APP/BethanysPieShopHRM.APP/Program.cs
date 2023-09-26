654786738c4e2bd29dcc6e8670f635736519d9dc
Sanyapoom Sirijirakarn <s.sirijirakarn@wsu.edu>
2023-09-26 15:44:44 -0700

    Add in Auth0 Authorization

--- BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Program.cs
+++ BethanysPieShopHRM.APP/BethanysPieShopHRM.APP/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using BethanysPieShopHRM.App.Services;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -19,5 +20,11 @@ builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(
 builder.Services.AddScoped<ApplicationState>();
 
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 
 await builder.Build().RunAsync();
